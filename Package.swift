543579b1e63d5b0b44dc99893b828e06ec106f30
Jonathan Guthrie <jono@perfect.org>
2016-12-20 16:02:22 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-15 19:55:52 +0000
+import PackageDescription
+let package = Package(
+    name: "Perfect-Session-SQLite-Demo",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 0, minor: 0),
+    ]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,33 @@
+
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import PerfectSession
+import PerfectSessionSQLite
+import SQLiteStORM
+import StORM
+
+let server = HTTPServer()
+
+SessionConfig.name = "TestingSQLiteDrivers"
+SessionConfig.idle = 10
+
+SQLiteConnector.db = "./SessionDB"
+
+StORMdebug = true
+
+let sessionDriver = SessionSQLiteDriver()
+
+server.setRequestFilters([sessionDriver.requestFilter])
+server.setResponseFilters([sessionDriver.responseFilter])
+
+server.addRoutes(makeWebDemoRoutes())
+server.serverPort = 8181
+
+do {
+	// Launch the HTTP server.
+	try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+	print("Network error thrown: \(err) \(msg)")
+}
