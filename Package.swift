96aa13971ba46f032899c6191f072409064851e1
Jonathan Guthrie <jono@perfect.org>
2017-01-12 16:59:45 -0500

    Update to 1.0, add CSRF tests

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-15 19:55:52 +0000
+// Date: 2017-01-12 21:53:58 +0000
 import PackageDescription
 let package = Package(
     name: "Perfect-Session-SQLite-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,21 @@
-
+//
+//  main.swift
+//  Perfect Session SQLite Demo
+//
+//  Created by Jonathan Guthrie on 2017-01-05.
+//	Copyright (C) 2017 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 20176 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
 
 import PerfectLib
 import PerfectHTTP
@@ -6,7 +23,6 @@ import PerfectHTTPServer
 import PerfectSession
 import PerfectSessionSQLite
 import SQLiteStORM
-import StORM
 
 let server = HTTPServer()
 
