9c106cf55a9da3815b2a805b83beb73827aac2fd
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:47:54 -0400

    update minor versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Perfect-Session-SQLite-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
     ]
 )
