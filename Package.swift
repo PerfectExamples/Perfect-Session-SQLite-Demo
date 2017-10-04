9f502e990168d47ee04dd57a3c7ee0f1df914a73
Jonathan Guthrie <jono@perfect.org>
2017-10-04 12:42:03 -0400

    Update to Perfect 3 / Swift 4

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 21:53:58 +0000
+// Date: 2017-10-04 16:34:35 +0000
 import PackageDescription
 let package = Package(
-    name: "Perfect-Session-SQLite-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-    ]
+	name: "Perfect-Session-SQLite-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-SQLite.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+	]
 )
